968b6ed171ff828012a516fca5128feab1252796
oxeq <xotaby4@yandex.ru>
2025-05-05 23:50:31 +0700

    feat: use ORJSONResponse as default response class

--- app/main.py
+++ app/main.py
@@ -2,9 +2,11 @@ import uvicorn
 
 from contextlib import asynccontextmanager
 from fastapi import FastAPI
+from fastapi.responses import ORJSONResponse
 
 from app.db.session import db_helper
 from app.core.config import settings
+from app.api import router as api_router
 
 
 @asynccontextmanager
@@ -16,7 +18,12 @@ async def lifespan(app: FastAPI):
 app = FastAPI(
     version="1.0",
     docs_url="/docs",
-    lifespan=lifespan
+    lifespan=lifespan,
+    default_response_class=ORJSONResponse
+)
+
+app.include_router(
+    api_router,
 )
 
 if __name__ == '__main__':

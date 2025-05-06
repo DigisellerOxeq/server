eeee2b9cc89e9ffdc409820c0b48b17a91afe604
oxeq <xotaby4@yandex.ru>
2025-05-06 20:38:05 +0700

    feat: create orders schema

---
+++ app/schemas/orders.py
@@ -0,0 +1,27 @@
+from typing import Optional
+from pydantic import BaseModel
+
+from enum import Enum
+
+# Статусы заказов
+class Status(str, Enum):
+    pending = "pending"
+    processing = "processing"
+    success = "success"
+    error = "error"
+
+
+# Получение информации о заказе
+class OrderRead(BaseModel):
+
+    id: int
+    inv: int
+    unique_code: str
+    lot_type: str
+    buyer_email: str
+    received: float
+    received_currency: str
+    pay_time: int
+    check_time: Optional[int] = None
+    status: Status
+    notation: Optional[str] = None

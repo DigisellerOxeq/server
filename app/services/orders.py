603dfb63ca2883925c6b0b6665698d103e879225
oxeq <xotaby4@yandex.ru>
2025-05-13 05:23:40 +0700

    feat: add search order method

--- app/services/orders.py
+++ app/services/orders.py
@@ -1,5 +1,8 @@
 from typing import Sequence
 
+from fastapi import HTTPException
+
+from app.integrations.digiseller import DigisellerAPI, DigisellerAPIError
 from app.repositories.orders import OrderRepository
 from app.schemas import OrderRead
 
@@ -11,8 +14,30 @@ class OrderService:
     async def get_all_orders(self) -> Sequence[OrderRead]:
         return await self.order_repo.get_all()
 
-    async def create_order(self, unique_code, digi_api):
-        if self.order_repo.get_by_unique_code(unique_code=unique_code):
-            return False
+    async def create_order(
+        self, unique_code: str, digi_api: DigisellerAPI
+    ) -> OrderRead:
+        if existing_order := await self.order_repo.get_by_unique_code(unique_code):
+            return existing_order
 
-        return None
+        try:
+            token = await digi_api.get_auth_token()
+            order_data = await digi_api.search_order(
+                unique_code=unique_code, token=token
+            )
+            print(order_data)
+            order = self._map_digi_response_to_order(unique_code, order_data)
+            return await self.order_repo.create(order)
+
+        except DigisellerAPIError as e:
+            raise HTTPException(
+                status_code=502, detail=f"Digiseller service unavailable: {str(e)}"
+            )
+
+    def _map_digi_response_to_order(
+        self, unique_code: str, digi_data: dict
+    ) -> OrderRead:
+        return OrderRead(
+            id=digi_data.get("order_id"),
+            unique_code=unique_code,
+        )

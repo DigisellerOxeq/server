1efb221294e8069d110c89d539c3c265fd4ecd09
oxeq <xotaby4@yandex.ru>
2025-05-13 05:23:54 +0700

    refactor: change token to api_key

--- app/dependencies/digiseller.py
+++ app/dependencies/digiseller.py
@@ -7,6 +7,6 @@ from app.core.config import settings
 async def get_digiseller_api(request: Request) -> DigisellerAPI:
     return DigisellerAPI(
         http_client=request.app.state.http_client,
-        token=settings.digi.token,
+        api_key=settings.digi.api_key,
         seller_id=settings.digi.seller_id,
     )

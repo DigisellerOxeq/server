8b106e71d6824e1fda91fa53a89ce8e15d183820
oxeq <xotaby4@yandex.ru>
2025-05-13 04:21:13 +0700

    feat: add depends digi

--- app/api/orders.py
+++ app/api/orders.py
@@ -1,10 +1,12 @@
 from fastapi import APIRouter, Query
 from fastapi.params import Depends
 
+from app.integrations.digiseller import DigisellerAPI
 from app.schemas.orders import OrderRead
 from app.services.orders import OrderService
 from app.dependencies.orders import get_order_service
 from app.dependencies.auth import get_auth
+from app.dependencies.digiseller import get_digiseller_api
 
 router = APIRouter(prefix="/orders", tags=["Orders"])
 
@@ -17,8 +19,9 @@ async def get_orders(service: OrderService = Depends(get_order_service)):
 
 # Создание заказа по уникальному коду Digiseller
 @router.post("/create")
-async def check_unique_code(
+async def create_order(
     service: OrderService = Depends(get_order_service),
-    unique_code: str = Query(default=..., max_length=30),
+    digi_api: DigisellerAPI = Depends(get_digiseller_api),
+    unique_code: str = Query(default=..., max_length=30)
 ):
-    return await service.create_order()
+    return await service.create_order(unique_code, digi_api)

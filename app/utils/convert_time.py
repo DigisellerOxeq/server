a2ddf19f72e14955be52fab2a30173bf696ac73f
oxeq <xotaby4@yandex.ru>
2025-05-13 20:16:01 +0700

    feat: add convert time utils

--- app/utils/convert_time.py
+++ app/utils/convert_time.py
@@ -1,9 +1,10 @@
 import pytz
 from datetime import datetime
 
+
 def moscow_to_timestamp(date: str) -> int:
-    date_format = '%d.%m.%Y %H:%M:%S'
-    moscow_tz = pytz.timezone('Europe/Moscow')
+    date_format = "%d.%m.%Y %H:%M:%S"
+    moscow_tz = pytz.timezone("Europe/Moscow")
     naive_datetime = datetime.strptime(date, date_format)
     localized_datetime = moscow_tz.localize(naive_datetime)
     timestamp = int(localized_datetime.timestamp())

4695bded6ad57b79e13bf4fb98af617bb912a27b
oxeq <test@.ru>
2025-05-25 14:43:42 +0700

    feat: add goods repo in order service

--- app/dependencies/orders.py
+++ app/dependencies/orders.py
@@ -3,11 +3,13 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.db.session import db_helper
 from app.repositories.orders import OrderRepository
+from app.repositories.goods import GoodsRepository
 from app.services.orders import OrderService
 
 
 def get_order_service(
     session: AsyncSession = Depends(db_helper.session_getter),
 ) -> OrderService:
-    repo = OrderRepository(session)
-    return OrderService(repo)
+    order_repo = OrderRepository(session)
+    goods_repo = GoodsRepository(session)
+    return OrderService(order_repo, goods_repo)
